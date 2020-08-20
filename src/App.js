cec984b3d7d16f3fbd4057aa7e33db8712c2a67b
Stanley Fils <stanleyfils@gmail.com>
2020-08-19 23:45:40 -0400

    Update home page

--- src/App.js
+++ src/App.js
@@ -3,28 +3,43 @@ import React, { Component } from "react";
 import { QuoteForm } from "./components/QuoteForm";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
 import AppBar from "material-ui/AppBar";
 import styled from "styled-components";
-// import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
+import { Link as RouterLink } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { borders } from "@material-ui/system";
+import "./App.css";
 
 const Title = styled.h1`
   color: midnightblue;
+  margin: 100px;
 `;
 
-class App extends Component {
-  render() {
-    return (
-      <MuiThemeProvider>
-        <AppBar
-          title={
-            <img src="https://coterieinsurance.com/static/rebrand_logo_dark-0f053ad962fde9064a4ed91fbd489f48.svg" />
-          }
-          showMenuIconButton={false}
-        />
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: "#b2ff59",
+    color: "midnightblue",
+    borderRadius: "30px",
+    "&:hover": {
+      color: "white",
+    },
+  },
+}));
 
+const App = () => {
+  const classes = useStyles();
+  return (
+    <MuiThemeProvider>
+      <nav>
+        <img
+          src="https://coterieinsurance.com/static/rebrand_logo_dark-0f053ad962fde9064a4ed91fbd489f48.svg"
+          alt="logo"
+        />
+      </nav>
+      <div className="main">
         <div>
           <Title>
             Simplifying
@@ -32,16 +47,14 @@ class App extends Component {
           </Title>
         </div>
 
-        <div>
-          <Card className="card">
-            <Button variant="contained" color="primary">
-              Get Your Quote Now
-            </Button>
-          </Card>
+        <div className="button">
+          <Button variant="contained" color="primary" className={classes.root}>
+            Get Your Quote Now
+          </Button>
         </div>
-      </MuiThemeProvider>
-    );
-  }
-}
+      </div>
+    </MuiThemeProvider>
+  );
+};
 
 export default App;
